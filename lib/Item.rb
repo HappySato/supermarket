8d9745fb0d1daad0435e1de7dca9fc68f853135d
Happy Sato <izumo.newspaper@gmail.com>
2016-01-31 15:27:02 +0900

    Add item check

---
+++ lib/Item.rb
@@ -0,0 +1,13 @@
+class Item
+  attr_accessor :name , :price
+  def initialize(name, price)
+  	@name = name
+  	@price = price
+  end
+  def name
+    @name
+  end
+  def price
+    @price
+  end
+end
